137cd166420d61f41ca33af1466e2cd32674380a
om0 <leo.needajob@gmail.com>
2018-08-10 11:43:02 +0800

    加入for loop

--- products.py
+++ products.py
@@ -5,4 +5,7 @@ while True:
 		break
 	price = input('請輸入商品價格: ')
 	products.append([name, price])
-print(products[0][1], products)
+# print(products[0][1], products)
+
+for p in products:
+	print(p[0], '的價格是', p[1])
